29c81a7393ca7eff035e60ef82988a5d0f1cc0aa
MohdFazil45 <mdfazil846@gmail.com>
2025-06-25 16:32:57 +0530

    Add Day 14 Good, Cheap, Fast Checkboxes Project

---
+++ Good, Cheap, Fast Checkboxes/readme.md
@@ -0,0 +1,3 @@
+Day 14 - Good, Cheap, Fast Toggle
+
+An interactive logic-based UI built using HTML, CSS, and JavaScript that demonstrates the classic developer constraint triangle: Good, Cheap, and Fast – you can only pick two!
---
+++ Good, Cheap, Fast Checkboxes/script.js
@@ -0,0 +1,21 @@
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change',(e) => 
+    doTheTrick(e.target)))
+
+function doTheTrick(theclickedOne) {
+    if(good.checked && cheap.checked && fast.checked){
+        if(good === theclickedOne) {
+            fast.checked = false
+        }
+        if(cheap === theclickedOne) {
+            good.checked = false
+        }
+        if(fast === theclickedOne) {
+            cheap.checked = false
+        }
+    }
+}
